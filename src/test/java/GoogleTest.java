4f66b997c90e7a95c49482c6c8f33bbef84fcd93
Abhishek Dixit <Sharmaabhidixit@gmail.com>
2026-06-18 16:28:06 +0530

    changes in jenkins

--- src/test/java/GoogleTest.java
+++ src/test/java/GoogleTest.java
@@ -1,5 +1,6 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.Test;
 
 public class GoogleTest {
@@ -7,7 +8,13 @@ public class GoogleTest {
     @Test
     public void testGoogle() {
 
-        WebDriver driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+
+        options.addArguments("--headless=new");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+
+        WebDriver driver = new ChromeDriver(options);
 
         driver.get("https://www.google.com");
 
